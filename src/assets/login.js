c3115b0ab2e4e193d373ae09ac1f8398fbfc0507
Pauloricardo1808 <pr243140@gmail.com>
2024-11-08 22:49:04 -0300

    atualizando login
    atualizando login js

--- src/assets/login.js
+++ src/assets/login.js
@@ -1,10 +1,8 @@
 function validarCampos() {
     const emailValido = validarEmail();
     const mensagemErros = document.querySelector('.erros');
-    const div = document.createElement('div');
-    const form = document.getElementById('forms');
+    document.getElementById('recuperarSenha').disabled = !emailValido;
     if (!emailValido) {
-        document.getElementById('recuperarSenha').disabled = !emailValido;
         mensagemErros.textContent = 'Insira um e-mail valido'
         mensagemErros.style.backgroundColor = 'rgb(255, 115, 115)'
     } else {
@@ -13,14 +11,6 @@ function validarCampos() {
     }
     const senha = validarSenha();
     document.getElementById('login').disabled = !senha || !emailValido;
-    if (!senha) {
-        form.appendChild(div);
-        div.innerHTML = '<h1>Insira uma senha valida</h1>'
-        div.style.backgroundColor = 'rgb(255, 115, 115)'
-    } else {
-        mensagemErros.textContent = '';
-        mensagemErros.style.backgroundColor = '#fff';
-    }
 }
 function emailValido (email) {
     let emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
