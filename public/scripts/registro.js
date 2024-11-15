4932eac8377d9b223cbb8c2c0ccaf0833e1cec19
Pauloricardo1808 <pr243140@gmail.com>
2024-11-15 14:42:07 -0300

    finalizando sistema de registro e login
    finalizando sistema de registro e login

--- public/scripts/registro.js
+++ public/scripts/registro.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById('forms');
 
 form.addEventListener('submit', async (event) => {
@@ -8,19 +7,31 @@ form.addEventListener('submit', async (event) => {
     const email = document.getElementById('exampleInputEmail1').value;
     const senha = document.getElementById('senha').value;
     const confirmaSenha = document.getElementById('confirmsenha').value;
-    
-    const response = await fetch('/registro', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ nome, email, senha, confirmaSenha })
-    });
-    
-    const message = await response.text();
-    alert(message);
+    const mensagemErro = document.getElementById('erros');
 
-    if (response.ok) {
-        window.location.href = '/login'; 
-    } else {
-        alert(message);
+    if (senha !== confirmaSenha) {
+        mensagemErro.textContent = "As senhas não correspondem!";
+        mensagemErro.style.display = "block";
+        return;
     }
+
+    try {
+        const response = await fetch('/registro', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nome, email, senha, confirmaSenha })
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            window.location.href = '/login';
+        } else {
+            mensagemErro.textContent = result.message;
+            mensagemErro.style.display = "block";
+        }
+    } catch (error) {
+        console.error('Erro ao enviar dados para o servidor:', error);
+        alert('Erro ao registrar. Tente novamente mais tarde.');
+    } 
 });
