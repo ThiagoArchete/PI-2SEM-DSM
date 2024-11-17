972d08598a27c6c084f0bcf8c0ad088550dcaf42
Thiago Archete <thiagocunha04@yahoo.com>
2024-11-17 20:55:09 -0300

    atualizando pagina de boards

--- public/scripts/taskflow.js
+++ public/scripts/taskflow.js
@@ -102,28 +102,76 @@ const $modal = document.getElementById('modal');
 const $descriptionInput = document.getElementById('description');
 const $priorityInput = document.getElementById('priority');
 const $deadlineInput = document.getElementById('deadline');
+const $columnInput = document.getElementById('column');
+const $idInput = document.getElementById("idInput");
 
-const $todoColumnBody = document.querySelector('#todoColumn .body')
+const $creationModeTitle = document.getElementById('creationModeTitle');
+const $editingModeTitle = document.getElementById('editingModeTitle');
 
-var todoList = [];
+const $creationModeBtn = document.getElementById('creationModeBtn');
+const $editingModeBtn = document.getElementById('editingModeBtn');
+
+var taskList = [];
 
 function openModal(id) {
     $modal.style.display = "flex";
+
+    if (id) {
+        $creationModeTitle.style.display = "none";
+        $creationModeBtn.style.display = "none";
+
+        $editingModeTitle.style.display = "block";
+        $editingModeBtn.style.display = "block";
+
+        const index = taskList.findIndex(function(task){
+            return task.id == id;
+        });
+
+    const task = taskList[index];
+
+    $idInput.value = task.id;
+    $descriptionInput.value = task.description;
+    $priorityInput.value = task.priority;
+    $deadlineInput.value = task.deadLine;
+    $columnInput.value = task.$column;
+    } else {
+        $creationModeTitle.style.display = "block";
+        $creationModeBtn.style.display = "block";
+
+        $editingModeTitle.style.display = "none";
+        $editingModeBtn.style.display = "none";
+    }
 }
 
 function closeModal() {
     $modal.style.display = "none";
 
+    $idInput.value = "";
     $descriptionInput.value = "";
     $priorityInput.value = "";
     $deadlineInput.value = "";
+    $columnInput.value = "";
 }
 
+function resetColumns() {
+    document.querySelector('[data-column="1"] .body').innerHTML = '';
+    document.querySelector('[data-column="2"] .body').innerHTML = '';
+    document.querySelector('[data-column="3"] .body').innerHTML = '';
+    document.querySelector('[data-column="4"] .body').innerHTML = '';
+}
+
+
 function generateCards() {
-    const todoListHtml = todoList.map(function(task) {
-        const formattedDate = moment(task.deadline).format('DD/MM/YYYY')
-        return `
-            <div class="card" ondblclick="openModal(${task.id})">
+
+    resetColumns();
+
+    taskList.forEach(function(task) {
+        const formattedDate = moment(task.deadLine).format('DD/MM/YYYY')
+
+        const columnBody = document.querySelector(`[data-column="${task.column}"] .body`);
+
+        const card = `
+            <div class="card" ondblclick="openModal(${task.id})" draggable="true">
                 <div class="info">
                     <b>Descrição:</b>
                     <span>${task.description}</span>
@@ -134,9 +182,9 @@ function generateCards() {
                 </div>
             </div>
         `;
-    });
 
-    $todoColumnBody.innerHTML = todoListHtml.join('');
+        columnBody.innerHTML += card;
+    });
 }
 
 function createTask() {
@@ -145,9 +193,29 @@ function createTask() {
         description: $descriptionInput.value,
         priority: $priorityInput.value,
         deadLine: $deadlineInput.value,
+        column: $columnInput.value,
     }
 
-    todoList.push(newTask);
+    taskList.push(newTask);
+
+    closeModal();
+    generateCards();
+}
+
+function updateTask() {
+    const task = {
+        id: $idInput.value,
+        description: $descriptionInput.value,
+        priority: $priorityInput.value,
+        deadLine: $deadlineInput.value,
+        column: $columnInput.value,
+    }
+
+    const index = taskList.findIndex(function(task){
+        return task.id == $idInput.value;
+    });
+
+    taskList[index] = task;
 
     closeModal();
     generateCards();
@@ -155,5 +223,39 @@ function createTask() {
 }
 
 
-
-
+function changeColumn(task_id, column_id) {
+    if (task_id && column_id) {
+      taskList = taskList.map((task) => {
+        if (task_id != task.id) return task;
+    
+        return {
+          ...task,
+          column: column_id,
+        };
+      });
+    }
+  
+    generateCards();
+  }
+  
+  function dragstart_handler(ev) {
+    console.log(ev);
+  
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("my_custom_data", ev.target.id);
+    ev.dataTransfer.effectAllowed = "move";
+  }
+  
+  function dragover_handler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  
+  function drop_handler(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    const task_id = ev.dataTransfer.getData("my_custom_data");
+    const column_id = ev.target.dataset.column;
+    
+    changeColumn(task_id, column_id);
+  }
