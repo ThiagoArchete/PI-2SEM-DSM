cc0f0684b8bca3d7da5c533216fd256e98b47f4e
Thiago Archete <thiagocunha04@yahoo.com>
2024-11-17 11:31:44 -0300

    atualizando pagina de board

--- public/scripts/taskflow.js
+++ public/scripts/taskflow.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.querySelector('.sidebar-section');
     const topBarName = document.querySelector('.selected-board-name');
     const boardInput = document.querySelector('.board-name-input');
+    const addBoardButton = document.querySelector('.add-board-button');
 
     function addBoard(name) {
         const newBoard = document.createElement('a');
@@ -16,13 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Escutar o evento de Enter no input para adicionar o novo quadro
+    // Mostrar o input ao clicar no botão "Adicionar novo quadro"
+    addBoardButton.addEventListener('click', () => {
+        boardInput.classList.remove('hidden');
+        boardInput.focus();
+    });
+
+    // Adicionar quadro ao pressionar Enter no input
     boardInput.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
             const boardName = boardInput.value.trim();
             if (boardName) {
                 addBoard(boardName);
                 boardInput.value = ''; // Limpar o campo de input
+                boardInput.classList.add('hidden'); // Ocultar o input novamente
             }
         }
     });
